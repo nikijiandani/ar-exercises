f0c47ea47adce7a8435c6ac60a8d03b5303053cd
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-09 19:22:05 -0400

    Add solution to Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@annual_revenue_sum = Store.sum("annual_revenue")
+puts "Total revenue storewide: $#{@annual_revenue_sum}"
+
+# output the average annual revenue for all stores
+@average_annual_revenue = @annual_revenue_sum / Store.count
+puts "Average annual revenue: $#{@average_annual_revenue}"
+
+# Output the number of stores that are generating $1M or more in annual sales
+puts "Number of stores that are generating $1M or more in annual sales: #{Store.where("annual_revenue > 1000000").count}"
