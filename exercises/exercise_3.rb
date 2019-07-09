2e8612617fbcd14feba51ae543b24651d55689b5
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-09 18:15:59 -0400

    Add solution to exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+
+puts "Going to destroy: #{@store3.name}"
+
+@store3.destroy
+
+puts "Updated store count after destruction of #{@store3.name}: #{Store.count}"
+
